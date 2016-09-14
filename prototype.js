c380b1877622f3d3a9499123c268e4122fcb3b15
yamadapc <tacla.yamada@gmail.com>
2016-09-13 23:07:45 -0300

    Update

--- prototype.js
+++ prototype.js
@@ -42,83 +42,86 @@ function patchLinks(posts) {
   return _.map(posts, patchLink);
 }
 
+function wrap(fn) {
+  const cfn = Promise.coroutine(fn);
+  return function generated(req, res, next) {
+    fn(req, res).catch(next);
+  };
+}
+
 app.use('/static', express.static('./static'));
 app.use('/themes', express.static('./views/themes'));
 
-app.get('/', (req, res, next) => {
-  Promise.coroutine(async () => {
-    const state = await steem.api.getStateAsync('/trending');
+app.get('/', wrap(async (req, res) => {
+  const state = await steem.api.getStateAsync('/trending');
 
-    res.render('home', {
-      appName,
-      latest: patchLinks(state.content),
-    });
-  })().then(null, next);
-});
+  res.render('home', {
+    appName,
+    latest: patchLinks(state.content),
+  });
+}));
 
 const themes = require('./themes');
-app.get('/:parent_permalink/:author/:permalink', (req, res, next) => {
-  Promise.coroutine(async () => {
-    const parent_permalink = req.params.permalink;
-    const author = req.params.author;
-    const permalink = req.params;
-    // console.log(
-    //   `/${parent_permalink}/${author}/${permalink}`
-    // );
+app.get('/:parent_permalink/:author/:permalink', wrap(async (req, res) => {
+  const parent_permalink = req.params.permalink;
+  const author = req.params.author;
+  const permalink = req.params;
+  // console.log(
+  //   `/${parent_permalink}/${author}/${permalink}`
+  // );
 
-    const state = await cachedGetStateAsync(
-        `/${parent_permalink}/@${author}/${permalink}`
-        );
-    const post = _.extend(state.content[author + '/' + permalink], {
-      body_html: marked(state.content[author + '/' + permalink].body, {
-        sanitize: true,
-        gfm: true,
-      }),
-    });
-    const meta = JSON.parse(post.json_metadata);
+  const state = await cachedGetStateAsync(
+    `/${parent_permalink}/@${author}/${permalink}`
+  );
+  const post = _.extend(state.content[author + '/' + permalink], {
+    body_html: marked(state.content[author + '/' + permalink].body, {
+      sanitize: true,
+      gfm: true,
+    }),
+  });
+  const meta = JSON.parse(post.json_metadata);
 
-    if (meta.theme && themes.byName[meta.theme]) {
-      const theme = themes.byName[meta.theme];
-      if (theme) {
-        const templates = await expressHandlebars.create({
-          extname: '.hbs'
-        }).getTemplates('./views/' + theme.filepath);
-        // console.log(post);
+  if (meta.theme && themes.byName[meta.theme]) {
+    const theme = themes.byName[meta.theme];
+    if (theme) {
+      const templates = await expressHandlebars.create({
+        extname: '.hbs'
+      }).getTemplates('./views/' + theme.filepath);
+      // console.log(post);
 
-        res.render(theme.filepath + '/post.hbs', {
-          appName,
-          post: _.extend(post, {
-            content: post.body_html,
-            author: {
-              name: author,
-              url: post.author_link,
-            },
-          }),
-          post_class: 'post',
-          partials: _.mapKeys(templates, (v, k) => path.basename(k, '.hbs')),
-          templates: _.mapKeys(templates, (v, k) => path.basename(k, '.hbs')),
-          layout: theme.filepath + '/default.hbs',
-          helpers: {
-            tags: () => meta.tags.join(', '),
-            asset: (s) => '/' + theme.filepath + '/assets/' + s,
-            date: () => moment(post.created).format('DD/MM/YYYY'),
-            encode: (s) => s,
-            url: (s) => s,
-            is: () => {},
-          }
-        });
-        return;
-      }
+      res.render(theme.filepath + '/post.hbs', {
+        appName,
+        post: _.extend(post, {
+          content: post.body_html,
+          author: {
+            name: author,
+            url: post.author_link,
+          },
+        }),
+        post_class: 'post',
+        partials: _.mapKeys(templates, (v, k) => path.basename(k, '.hbs')),
+        templates: _.mapKeys(templates, (v, k) => path.basename(k, '.hbs')),
+        layout: theme.filepath + '/default.hbs',
+        helpers: {
+          tags: () => meta.tags.join(', '),
+          asset: (s) => '/' + theme.filepath + '/assets/' + s,
+          date: () => moment(post.created).format('DD/MM/YYYY'),
+          encode: (s) => s,
+          url: (s) => s,
+          is: () => {},
+        }
+      });
+      return;
     }
+  }
 
-    // console.log(post)
+  // console.log(post)
 
-    res.render('post', {
-      appName,
-      post,
-    });
-  }).then(null, next);
-});
+  res.render('post', {
+    appName,
+    post,
+  });
+}));
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
